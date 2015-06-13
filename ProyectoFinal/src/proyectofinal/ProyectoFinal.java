649b16adb339273e905c005927ecbbb645ada213
WilberparionaHuaraca <deivisspariona@gmail.com>
2015-06-13 11:31:16 -0500

    Update ProyectoFinal.java

--- ProyectoFinal/src/proyectofinal/ProyectoFinal.java
+++ ProyectoFinal/src/proyectofinal/ProyectoFinal.java
@@ -16,7 +16,7 @@ public class ProyectoFinal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Wilber  pariona huaraca ");
+        System.out.println("Wilber  pariona  huaraca ");
        
     
         
